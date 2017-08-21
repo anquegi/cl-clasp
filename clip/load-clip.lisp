a3ab964e88f90200400a994fdfdab639788cf1a2
Toni <antonio.querol@cuaqea.com>
2017-08-21 17:31:16 +0200

    extract tgz and remove

---
+++ clip/load-clip.lisp
@@ -0,0 +1,67 @@
+;;;; -*- Mode:Common-Lisp; Package:USER; Fonts:(MEDFNT); Base:10 -*-
+;;;; *-* File: Titanic: /usr/users/eksl/mac-files/clasp/load-clasp.lisp *-*
+;;;; *-* Last-edit: Monday, March 15, 1993  12:08:41; Edited-By: Westy *-* 
+;;;; *-* Machine: Count (Explorer II, Microcode 489) *-*
+;;;; *-* Software: TI Common Lisp System 6.49 *-*
+;;;; *-* Lisp: TI Common Lisp System 6.49  *-*
+
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;; *                                                                        *
+;;;; *                           Load File for CLIP                           *
+;;;; *                                                                        *
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;
+;;; Copyright (c) 1994 University of Massachusetts
+;;; Department of Computer Science
+;;; Experimental Knowledge Systems Laboratory
+;;; Professor Paul Cohen, Director.
+;;; All rights reserved.
+
+;;; Permission to use, copy, modify and distribute this software and its
+;;; documentation is hereby granted without fee for non-commercial uses
+;;; only (not for resale), provided that the above copyright notice of EKSL,
+;;; this paragraph and the one following appear in all copies and in
+;;; supporting documentation.
+
+;;; EKSL makes no representation about the suitability of this software for any
+;;; purposes.  It is provided "AS IS", without express or implied warranties
+;;; including (but not limited to) all implied warranties of merchantability
+;;; and fitness for a particular purpose, and notwithstanding any other
+;;; provision contained herein.  In no event shall EKSL be liable for any
+;;; special, indirect or consequential damages whatsoever resulting from loss
+;;; of use, data or profits, whether in an action of contract, negligence or
+;;; other tortuous action, arising out of or in connection with the use or
+;;; performance of this software, even if EKSL is advised of the possiblity
+;;; of such damages.
+
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;;;
+;;;  03-09-93 File Created.  (Westy)
+;;;
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;;; --*--
+
+(in-package #+CLTL2 :COMMON-LISP-USER #-CLTL2 :USER)
+
+;;; --*--
+;;; ***************************************************************************
+
+(load (merge-pathnames "load-utils"  
+                       #+lucid lcl:*source-pathname*
+                       #+allegro excl:*source-pathname*
+                       #+(or Genera Explorer) sys:fdefine-file-pathname
+                       #-(or lucid allegro xerox Genera Explorer) *load-pathname*))
+
+;;;----------------------------------------------------------------------------
+;;; Load-time stuff
+
+(unless *clip-version-used* (set-clip-version))
+
+(load-clip)
+
+(clip-copyright-notice)
+
+;;; ***************************************************************************
+;;; EOF
